ecf2fe372db5cc489bd4d52d9483bdb068fcecdc
davidsherry14 <davidsherry1407@gmail.com>
2024-12-09 14:24:36 +0000

    updates

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const title = (document.getElementById("title"));
+const title = document.getElementById("title");
 console.log(title.textContent);
 
 const content = document.getElementsByClassName("content");
 for (let index = 0; index < content.length; index++) {
-    console.log(content[index].textContent)
+  console.log(content[index].textContent);
 }
 
 const listEls = document.getElementsByTagName("li");
 for (let index = 0; index < listEls.length; index++) {
-    console.log(listEls[index].textContent)
+  console.log(listEls[index].textContent);
 }
 
 const highlight = document.querySelector(".highlight");
@@ -16,7 +16,7 @@ console.log(highlight.textContent);
 
 const highlightAll = document.querySelectorAll(".highlight");
 for (let index = 0; index < highlightAll.length; index++) {
-    console.log(highlightAll[index].textContent)
+  console.log(highlightAll[index].textContent);
 }
 
 //Returns first child of the li
@@ -29,5 +29,13 @@ console.log(ParNode.textContent);
 
 const itemList = document.getElementById("itemList").childNodes;
 for (let index = 0; index < itemList.length; index++) {
-    console.log(itemList[index].textContent)
+  console.log(itemList[index].textContent);
 }
+
+const itemUl = document.getElementById("itemList").children;
+console.log(itemUl);
+
+const firstLast = document.getElementById("itemList").firstChild;
+console.log(firstLast.textContent);
+const lastFirst = document.getElementById("itemList").lastChild;
+console.log(lastFirst.textContent);
