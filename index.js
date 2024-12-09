ed7dfeb311c9f53f01b7e18e2ed9ef0c8fca7e37
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2024-12-09 13:32:44 +0000

    Tickets 1a to 1c

---
+++ index.js
@@ -0,0 +1,14 @@
+const title = document.getElementById("title");
+console.log(title.textContent)
+
+const content = document.getElementsByClassName("content");
+console.log(content[0].textContent)
+console.log(content[1].textContent)
+
+const list = document.getElementsByTagName("li")
+console.log(list[0].textContent)
+console.log(list[1].textContent)
+console.log(list[2].textContent)
+console.log(list[3].textContent)
+console.log(list[4].textContent)
+
